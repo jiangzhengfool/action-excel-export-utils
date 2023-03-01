77433d16955244a728c9f21b4eba404a369b4d78
xuml <xuml@abc-group.cn>
2023-03-01 16:49:45 +0800

    init: 初始项目代码

---
+++ src/test/java/com/feiyizhan/excel/export/utils/ExcelExportUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiyizhan.excel.export.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelExportUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
